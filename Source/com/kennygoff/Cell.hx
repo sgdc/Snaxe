cbcf2ba07a401ea2b00d562848c3a9567d6d8f23
Kenny Goff <goff.kenneth@gmail.com>
2013-09-17 13:21:26 -0400

    Implement food and snake growth

--- Source/com/kennygoff/Cell.hx
+++ Source/com/kennygoff/Cell.hx
@@ -2,17 +2,22 @@ package com.kennygoff;
 
 class Cell extends Entity
 {
+  var rendered : Bool;
   function new(?x:Int, ?y:Int, ?width:Int, ?height:Int)
   {
     super();
 
+    rendered = false;
     this.x = (x == null)? 0 : x;
     this.y = (y == null)? 0 : y;
-    this._initDimensions((width == null)? 20 : width, (height == null)? 20 : height);
+    this._initDimensions((width == null)? 10 : width, (height == null)? 10 : height);
   }
 
   public override function render()
   {
+    if(rendered) return;
+    rendered = true;
+    
     this.graphics.beginFill(0x555555, 1);
     this.graphics.drawRect(0, 0, this.width, this.height);
     this.graphics.endFill();
--- Source/com/kennygoff/Snaxe.hx
+++ Source/com/kennygoff/Snaxe.hx
@@ -2,11 +2,23 @@ package com.kennygoff;
 
 import flash.display.Sprite;
 import flash.events.Event;
+import flash.events.KeyboardEvent;
+import flash.ui.Keyboard;
 import openfl.display.FPS;
+import haxe.Timer;
+
+enum Direction
+{
+  Up; Right; Down; Left;
+}
 
 class Snaxe extends Sprite
 {
-  var snake : Snake;
+  var snake : Array<Cell>;
+  var direction : Direction;
+  var snakeLength : Int;
+  var cacheTime : Float;
+  var food : Cell;
 
   public function new()
   {
@@ -18,12 +30,47 @@ class Snaxe extends Sprite
   public function init(e:Event)
   {
     this.removeEventListener(Event.ADDED_TO_STAGE, init);
-    this.addEventListener(Event.ENTER_FRAME, update);
 
-    snake = new Snake(20, 20);
-    addChild(snake);
-
+    cacheTime = Timer.stamp();
     addChild(new FPS(0, 0));
+    initSnake();
+    initFood();
+
+    this.addEventListener(Event.ENTER_FRAME, update);
+    stage.addEventListener(KeyboardEvent.KEY_DOWN, stage_onKeyDown);
+  }
+
+  public function initFood()
+  {
+    if(food == null)
+    {
+      food = new Cell(0, 0, 10, 10);
+      addChild(food);
+    }
+
+    var fx = Math.floor(Math.random() * 48);
+    var fy = Math.floor(Math.random() * 48);
+
+    food.x = fx*10;
+    food.y = fy*10;
+  }
+
+  public function initSnake()
+  {
+    if(snake != null)
+    {
+      for(cell in snake)
+      {
+        removeChild(cell);
+      }
+    }
+
+    direction = Right;
+    snakeLength = 5;
+    snake = new Array<Cell>();
+    var cell = new Cell(0, 0);
+    snake.push(cell);
+    addChild(cell);
   }
 
   public function clear()
@@ -35,10 +82,60 @@ class Snaxe extends Sprite
 
   public function update(e:Event)
   {
-    snake.update(e);
+    var currentTime = Timer.stamp();
+    if(currentTime - cacheTime > 0.05)
+    {
+      cacheTime = currentTime;
+
+      var tail = snake.pop();
+      var tail_x = Math.floor(tail.x);
+      var tail_y = Math.floor(tail.y);
+      if(snake.length > 0)
+      {
+        tail.x = snake[0].x;
+        tail.y = snake[0].y;
+      }
+      switch(this.direction)
+      {
+        case Up: tail.y -= 10;
+        case Right: tail.x += 10;
+        case Down: tail.y += 10;
+        case Left: tail.x -= 10;
+      }
+
+      snake.unshift(tail);
+
+      if(snake.length < snakeLength)
+      {
+        var cell = new Cell(tail_x, tail_y);
+        snake.push(cell);
+        addChild(cell);
+      }
+
+      if(snake.length > 0 && food != null && Math.floor(snake[0].x/10) == Math.floor(food.x/10) && Math.floor(snake[0].y/10) == Math.floor(food.y/10))
+      {
+        snakeLength++;
+        initFood();
+      }
+    }
 
     // render
     clear();
-    snake.render();
+    for(cell in snake)
+    {
+      cell.render();
+    }
+    food.render();
+  }
+
+  private function stage_onKeyDown(e:KeyboardEvent)
+  {
+    switch (e.keyCode)
+    {
+      case Keyboard.UP: direction = Up;
+      case Keyboard.RIGHT: direction = Right;
+      case Keyboard.DOWN: direction = Down;
+      case Keyboard.LEFT: direction = Left;
+    }
   }
 }
